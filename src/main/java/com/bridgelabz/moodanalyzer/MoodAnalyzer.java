af3675ae42e2539b9fc0b35e12ec552d85acba5a
Naveen2706 <naveen271996@gmail.com>
2021-07-18 20:04:12 +0530

    MoodAnalyser with exceptions

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyzer {
+
+    private String message; // define variable
+
+    public MoodAnalyzer(String message) { // define parameterized constructor
+        this.message = message;
+    }
+
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message == "")
+                throw new MoodAnalysisException("Invalid message", MoodAnalysisException.ExceptionType.ENTERED_EMPTY);
+            if (message.toLowerCase().contains("sad"))
+                return "SAD";
+            return "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalysisException("Invalid message", MoodAnalysisException.ExceptionType.ENTERED_NULL);
+        }
+    }
+}
---
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,53 @@
+package com.bridgelabz.moodanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+    /*
+     * This Test Case Excepts Sad Mood
+     */
+    @Test
+    public void givenMessage_whenSad_ShouldReturn_Sad() throws MoodAnalysisException {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        String actual = moodAnalyzer.analyseMood();
+        String expected = "SAD";
+        Assert.assertEquals(expected, actual);
+    }
+
+    /*
+     * This Test Case Excepts Happy Mood
+     */
+    @Test
+    public void givenMessage_whenNotSad_ShouldReturn_Happy() throws MoodAnalysisException {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Happy Mood");
+        String actual = moodAnalyzer.analyseMood();
+        String expected = "HAPPY";
+        Assert.assertEquals(expected, actual);
+    }
+
+    /*
+     * This Test Case Excepts Null Mood
+     */
+    @Test
+    public void givenNullMood_ShouldReturn_Happy() throws MoodAnalysisException {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        String actual = moodAnalyzer.analyseMood();
+        String expected = "HAPPY";
+        Assert.assertEquals(expected, actual);
+    }
+    /*
+     * This Test Case Will Check For empty and values
+     */
+
+    @Test
+    public void givenNullMood_ShouldThrow_Exception() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        try {
+            moodAnalyzer.analyseMood();
+        } catch (MoodAnalysisException e) {
+            System.out.println("here");
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.exceptionType);
+        }
+    }
+}
